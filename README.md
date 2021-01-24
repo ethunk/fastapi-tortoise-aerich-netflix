73f33d39cf4d94873c62e1937edd03ae44a38e9a
Eric <eric@coherentpath.com>
2021-01-23 23:10:02 -0700

    updated readme and create_db_records

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
 # FastAPI + Tortoise-ORM + Aerich
 
 ## To run locally
-You will need to set the environment variable for `DATABASE_URL`
+You will need to set the environment variable for `DATABASE_URL` either in a .env file or explicitly in the Dockerfile
 
-Option 1:
-- Docker build image locally and run
+Then there are the following options to run locally.
 
-Option 2:
--
-- Create a python virtual environment
-- pip install -r ./requirements.txt
+- Option 1:
+  - Docker build image locally and run
+
+- Option 2:
+  - Create a python virtual environment (.tool-versions specifies python 3.7.1, recommend using asdf)
+  - `pip install -r ./requirements.txt`
+  - Setup the database (optional, can be handled by tortoise ORM if starting from scratch)
+```
+aerich init -t app.db.TORTOISE_ORM
+aerich init-db
+aerich migrate
+aerich upgrade
+```
+  - You can pip install pandas and ipython. Then start an ipython session and copy and paste the code from the `create_data_records.py` file to populate your database
+  - run `uvicorn app.main:app --reload`
+  - visit: `localhost:8000/` or `127.0.0.1:8000/`
+
+
+Running Test:
+`pytest`
+```
+╰➜ pytest
+============================================================ test session starts ============================================================
+platform darwin -- Python 3.7.1, pytest-6.2.1, py-1.10.0, pluggy-0.13.1
+rootdir: /Users/erichunkler/Documents/projects/fastapi-tortoise-aerich-netflix
+plugins: asyncio-0.14.0
+collected 3 items
+
+app/test_api.py ...                                                                                                                   [100%]
+
+============================================================= 3 passed in 2.52s =============================================================
+```
--- create_database_records.py
+++ create_database_records.py
@@ -5,43 +5,43 @@ from app.models import Content, ContentType, Rating
 from datetime import datetime
 from tortoise.exceptions import IntegrityError
 from tortoise import Tortoise
+from tortoise import run_async
 
 DATE_FORMAT = '%d-%b-%y'
 
 async def init():
-     # Here we connect to a DATABASE_URL file.
-     # also specify the app name of "models"
-     # which contain models from "app.models"
-     await Tortoise.init(
-         db_url=os.environ.get("DATABASE_URL"),
-         modules={'models': ['app.models']}
-     )
-     # Generate the schema
-     await Tortoise.generate_schemas()
+    # Here we connect to a DATABASE_URL file.
+    # also specify the app name of "models"
+    # which contain models from "app.models"
+    await Tortoise.init(
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={'models': ['app.models']}
+    )
+    # Generate the schema
+    await Tortoise.generate_schemas()
 
-await init()
+    await init()
 
-df = pd.read_csv('netflix_titles.csv')
-records = df.to_records(index=False)
+    df = pd.read_csv('netflix_titles.csv')
+    records = df.to_records(index=False)
 
-new_items_list = []
-for record in records:
-    id_, type_, title, date_added, release_year, rating, duration, description = record
-    try:
-        new_item = Content(
-            # id=id_,
-            type=ContentType._value2member_map_.get(type_),
-            title=title,
-            date_added=datetime.strptime(date_added, DATE_FORMAT) if date_added == date_added else None,
-            rating=Rating._value2member_map_.get(rating),
-            duration=int(duration.strip('minseason')) if duration else None,
-            description=description,
-            release_year=release_year if release_year else None,
-        )
-        new_items_list.append(new_item)
-    except (ValueError, IntegrityError):
-        continue
+    new_items_list = []
+    for record in records:
+        id_, type_, title, date_added, release_year, rating, duration, description = record
+        try:
+            new_item = Content(
+                # id=id_,
+                type=ContentType._value2member_map_.get(type_),
+                title=title,
+                date_added=datetime.strptime(date_added, DATE_FORMAT) if date_added == date_added else None,
+                rating=Rating._value2member_map_.get(rating),
+                duration=int(duration.strip('minseason')) if duration else None,
+                description=description,
+                release_year=release_year if release_year else None,
+            )
+            new_items_list.append(new_item)
+        except (ValueError, IntegrityError):
+            continue
+    await Content.bulk_create(new_items_list)
 
-await Content.bulk_create(new_items_list)
-
-await Tortoise.close_connections()
+    await Tortoise.close_connections()
